04d1b3f5f5f8b4fdbe5cff9e6912b9a3eb6a6231
Joel Thomas <joel@codewithjoe.in>
2025-06-07 12:58:41 +0530

    created the new superuser

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9yyxq0_sq3)zlqc9cm4xef#+h4q5%y3b8w7aztpxnz23_x!8yw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["bw-one.codewithjoe.in"]
 
